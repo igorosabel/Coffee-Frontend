0c5d151599bbf7eba82da1cad001b9fced6bb125
Iñigo Gorosabel <inigo.gorosabel@gmail.com>
2018-08-24 13:12:22 +0200

    Actualizo a Angular 6

--- src/app/services/dialog.service.ts
+++ src/app/services/dialog.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs/Observable';
+import { Observable } from 'rxjs';
 import { MatDialogRef, MatDialog } from '@angular/material';
 import { ConfirmDialogComponent } from './dialogs/confirm-dialog.component';
 import { AlertDialogComponent } from './dialogs/alert-dialog.component';
