66247f65ddbc5ca6963bf1a3ccc54a76a585eace
Iñigo Gorosabel <inigo.gorosabel@gmail.com>
2020-11-17 17:47:21 +0100

    Quito dependencia flex-layout

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,6 @@
 import { BrowserModule }           from '@angular/platform-browser';
 import { NgModule }                from '@angular/core';
 import { HttpClientModule }        from '@angular/common/http';
-import { FlexLayoutModule }        from '@angular/flex-layout';
 import { FormsModule }             from '@angular/forms';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule }        from './app-routing.module';
@@ -16,27 +15,26 @@ import { ServiceWorkerModule } from '@angular/service-worker';
 import { environment } from '../environments/environment';
 
 @NgModule({
-  declarations: [
+	declarations: [
 		AppComponent,
 		...PAGES,
 		...COMPONENTS,
 		...PIPES
-  ],
-  imports: [
+	],
+	imports: [
 		BrowserModule,
 		AppRoutingModule,
 		BrowserAnimationsModule,
 		HttpClientModule,
 		FormsModule,
-		FlexLayoutModule,
 		ColorPickerModule,
 		...MATERIAL,
 		ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production })
-  ],
-  entryComponents: [ConfirmDialogComponent, AlertDialogComponent],
-  providers: [
+	],
+	entryComponents: [ConfirmDialogComponent, AlertDialogComponent],
+	providers: [
 		...SERVICES
 	],
-  bootstrap: [AppComponent]
+	bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
--- src/app/components/calendar/calendar.component.ts
+++ src/app/components/calendar/calendar.component.ts
@@ -1,160 +1,151 @@
 import { Component, OnInit, Output, EventEmitter } from '@angular/core';
-import { Subscription }                            from 'rxjs';
-import { MediaChange, MediaObserver }              from '@angular/flex-layout';
 
 @Component({
-  selector: 'calendar',
-  templateUrl: './calendar.component.html',
-  styleUrls: ['./calendar.component.css']
+	selector: 'calendar',
+	templateUrl: './calendar.component.html',
+	styleUrls: ['./calendar.component.css']
 })
 export class CalendarComponent implements OnInit {
-  @Output() select = new EventEmitter();
-  @Output() change = new EventEmitter();
+	@Output() select = new EventEmitter();
+	@Output() change = new EventEmitter();
 
-  months = ['Urtarrila','Otsaila','Martxoa','Apirila','Maiatza','Ekaina','Uztaila','Abuztua','Iraila','Urria','Azaroa','Abendua'];
-  monthsShort = ['Urt','Ots','Mar','Api','Mai','Eka','Uzt','Abu','Ira','Urr','Aza','Abe'];
-  days = ['Astelehena','Asteartea','Asteazkena','Osteguna','Ostirala','Larunbata','Igandea'];
-  daysShort = ['Asl','Ast','Asz','Ost','Osi','Lar','Iga'];
+	months = ['Urtarrila','Otsaila','Martxoa','Apirila','Maiatza','Ekaina','Uztaila','Abuztua','Iraila','Urria','Azaroa','Abendua'];
+	//monthsShort = ['Urt','Ots','Mar','Api','Mai','Eka','Uzt','Abu','Ira','Urr','Aza','Abe'];
+	days = ['Astelehena','Asteartea','Asteazkena','Osteguna','Ostirala','Larunbata','Igandea'];
+	//daysShort = ['Asl','Ast','Asz','Ost','Osi','Lar','Iga'];
 
-  day = null;
-  month = null;
-  year = null;
-  events = [];
-  marked = {};
+	day = null;
+	month = null;
+	year = null;
+	events = [];
+	marked = {};
 
-  currentMonth = null;
-  currentYear = null;
-  headerDays = [];
-  rows = [];
-  
-  watcher: Subscription;
-  isMobile: boolean = false;
+	currentMonth = null;
+	currentYear = null;
+	headerDays = [];
+	rows = [];
 
-  constructor(private mediaObserver: MediaObserver) {
-	this.watcher = mediaObserver.media$.subscribe((change: MediaChange) => {
-      if ( change.mqAlias == 'xs') {
-         this.isMobile = true;
-      }
-    });
-  }
+	constructor() {}
 
-  ngOnInit() {
-    this.isMobile = this.mediaObserver.isActive('xs');
-  }
+	ngOnInit() {}
 
-  selectDay(d){
-    this.select.emit({day: d.day, month: this.month+1, year: this.year});
-  }
+	selectDay(d) {
+		this.select.emit({day: d.day, month: this.month+1, year: this.year});
+	}
 
-  setDate(d){
-	  this.day = d.day;
-	  this.month = d.month;
-	  this.year = d.year;
-  }
+	setDate(d) {
+		this.day = d.day;
+		this.month = d.month;
+		this.year = d.year;
+	}
 
-  getDate(){
-    return {day: this.day, month: this.month, year: this.year};
-  }
+	getDate() {
+		return {day: this.day, month: this.month, year: this.year};
+	}
 
-  setEvents(events){
-    this.rows = [];
-    this.events = events;
-    this.marked = {};
-    this.events.forEach(element => this.marked[element.d] = element);
-  }
+	setEvents(events) {
+		this.rows = [];
+		this.events = events;
+		this.marked = {};
+		this.events.forEach(element => this.marked[element.d] = element);
+	}
 
-  previousMonth(ev){
-    ev.preventDefault();
-    this.month--;
-    if (this.month==-1){
-      this.month = 11;
-      this.year--;
-    }
-    this.setEvents([]);
-    this.change.emit(this.getDate());
-  }
+	previousMonth(ev) {
+		ev.preventDefault();
+		this.month--;
+		if (this.month==-1) {
+			this.month = 11;
+			this.year--;
+		}
+		this.setEvents([]);
+		this.change.emit(this.getDate());
+	}
 
-  nextMonth(ev){
-    ev.preventDefault();
-    this.month++;
-    if (this.month==12){
-      this.month = 0;
-      this.year++;
-    }
-    this.setEvents([]);
-    this.change.emit(this.getDate());
-  }
+	nextMonth(ev) {
+		ev.preventDefault();
+		this.month++;
+		if (this.month==12) {
+			this.month = 0;
+			this.year++;
+		}
+		this.setEvents([]);
+		this.change.emit(this.getDate());
+	}
 
-  header(){
-    const months = this.isMobile ? this.monthsShort : this.months;
-    const days   = this.isMobile ? this.daysShort   : this.days;
+	header() {
+		//const months = this.isMobile ? this.monthsShort : this.months;
+		//const days   = this.isMobile ? this.daysShort   : this.days;
 
-    this.currentMonth = months[this.month];
-    this.currentYear = this.year;
-    this.headerDays = days;
-  }
+		this.currentMonth = this.months[this.month];
+		this.currentYear = this.year;
+		this.headerDays = this.days;
+	}
 
-  otherMonthDay(day){
-    return {class:'calendar-day-other', day: day};
-  }
+	otherMonthDay(day) {
+		return {class:'calendar-day-other', day: day};
+	}
 
-  currentMonthDay(day){
-    const now = new Date();
-    let today = (this.year===now.getFullYear() && this.month===now.getMonth() && this.day===day) ? ' calendar-today' : '';
-    let marked = '';
-    if (this.marked[day] && this.marked[day].list && this.marked[day].list.length==1){
-      marked = ' person_' + this.marked[day].list[0].id_person;
-    }
-    return {class:'calendar-day calendar-clickable'+today+marked, day: day, events: this.marked[day]};
-  }
+	currentMonthDay(day) {
+		const now = new Date();
+		let today = (this.year===now.getFullYear() && this.month===now.getMonth() && this.day===day) ? ' calendar-today' : '';
+		let marked = '';
+		if (this.marked[day] && this.marked[day].list && this.marked[day].list.length==1) {
+			marked = ' person_' + this.marked[day].list[0].id_person;
+		}
+		return {
+			class: 'calendar-day calendar-clickable' + today + marked,
+			day: day,
+			events: this.marked[day]
+		};
+	}
 
-  draw(){
-    // Obtengo el primer día del mes y el primer día de la semana
-    const firstDay = new Date(this.year, this.month, 1);
-    const firstDayWeekday = firstDay.getDay() === 0 ? 7 : firstDay.getDay();
+	draw() {
+		// Obtengo el primer día del mes y el primer día de la semana
+		const firstDay = new Date(this.year, this.month, 1);
+		const firstDayWeekday = firstDay.getDay() === 0 ? 7 : firstDay.getDay();
 
-    // Obtengo número de días en el mes
-    const monthLength = new Date(this.year, this.month+1, 0).getDate();
-    const previousMonthLength = new Date(this.year, this.month, 0).getDate();
+		// Obtengo número de días en el mes
+		const monthLength = new Date(this.year, this.month+1, 0).getDate();
+		const previousMonthLength = new Date(this.year, this.month, 0).getDate();
 
-    // Cabecera del calendario
-    this.header();
+		// Cabecera del calendario
+		this.header();
 
-    // Contenido del calendario
-    // Variables con valores por defecto para los días
-    let day  = 1; // Día actual del mes
-    let prev = 1; // Días del mes anterior
-    let next = 1; // Días del mes siguiente
-	  let nextMonth = false;
+		// Contenido del calendario
+		// Variables con valores por defecto para los días
+		let day  = 1; // Día actual del mes
+		let prev = 1; // Días del mes anterior
+		let next = 1; // Días del mes siguiente
+		let nextMonth = false;
 
-    // Bucle de semanas (filas)
-    for (let i = 0; i < 9; i++){
-	  if (nextMonth){ continue; }
-	  let days = [];
+		// Bucle de semanas (filas)
+		for (let i = 0; i < 9; i++) {
+			if (nextMonth) { continue; }
+			let days = [];
 
-      // Bucle días de la semana (celdas)
-      for (let j = 1; j <= 7; j++){
-        if (day <= monthLength && (i > 0 || j >= firstDayWeekday)){
-          // Mes actual
-          days.push( this.currentMonthDay(day) );
-          day++;
-        }
-        else{
-          if (day <= monthLength) {
-            // Mes anterior
-            days.push( this.otherMonthDay( previousMonthLength - firstDayWeekday + prev + 1 ) );
-            prev++;
-          }
-          else{
-            // Mes siguiente
-            days.push( this.otherMonthDay(next) );
-            next++;
-			      nextMonth = true;
-          }
-        }
-      }
+			// Bucle días de la semana (celdas)
+			for (let j = 1; j <= 7; j++) {
+				if (day <= monthLength && (i > 0 || j >= firstDayWeekday)) {
+					// Mes actual
+					days.push( this.currentMonthDay(day) );
+					day++;
+				}
+				else {
+					if (day <= monthLength) {
+						// Mes anterior
+						days.push( this.otherMonthDay( previousMonthLength - firstDayWeekday + prev + 1 ) );
+						prev++;
+					}
+					else {
+						// Mes siguiente
+						days.push( this.otherMonthDay(next) );
+						next++;
+						nextMonth = true;
+					}
+				}
+			}
 
-	    this.rows.push(days);
-    }
-  }
+			this.rows.push(days);
+		}
+	}
 }
